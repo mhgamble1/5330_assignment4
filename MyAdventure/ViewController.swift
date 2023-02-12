1ffb3754dd90fb049c09edd261f2e87b1bead1ad
mhgamble1 <mhgamble1@gmail.com>
2023-02-12 17:25:47 -0600

    add logic for the end of game

--- MyAdventure/ViewController.swift
+++ MyAdventure/ViewController.swift
@@ -8,34 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var Prompt: UILabel!
-    @IBOutlet weak var leftButton: UIButton!
-    @IBOutlet weak var rightButton: UIButton!
-    
-    var gameLogic = GameLogic()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        updateUI()
-        // Do any additional setup after loading the view.
-    }
+  @IBOutlet weak var Prompt: UILabel!
+  @IBOutlet weak var leftButton: UIButton!
+  @IBOutlet weak var rightButton: UIButton!
 
-    
-    @IBAction func leftButtonSubmit(_ sender: Any) {
+  var gameLogic = GameLogic()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    updateUI()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func leftButtonSubmit(_ sender: Any) {
+    if gameLogic.isGameOver() {
+      // restart the game
+      gameLogic.restart()
+    } else {
         gameLogic.pickLeft()
-        updateUI()
     }
-    
-    @IBAction func rightButtonSubmit(_ sender: Any) {
-        gameLogic.pickRight()
-        updateUI()
-    }
-    
-    func updateUI() {
-        Prompt.text = gameLogic.getNextPrompt()
-        leftButton.setTitle(gameLogic.getNextLeft(), for: .normal)
-        rightButton.setTitle(gameLogic.getNextRight(), for: .normal)
-    }
-    
-}
+    updateUI()
+  }
 
+  @IBAction func rightButtonSubmit(_ sender: Any) {
+    if gameLogic.isGameOver() {
+      // restart the game
+      gameLogic.restart()
+    } else {
+        gameLogic.pickRight()
+    }
+    updateUI()
+  }
+
+  func updateUI() {
+    if gameLogic.isGameOver() {
+      leftButton.isHidden = true
+      rightButton.isHidden = true
+      Prompt.text = gameLogic.getOutcome()
+    } else {
+      Prompt.text = gameLogic.getPrompt()
+      leftButton.setTitle(gameLogic.getLeftButton(), for: .normal)
+      rightButton.setTitle(gameLogic.getRightButton(), for: .normal)
+    }
+  }
+
+}
